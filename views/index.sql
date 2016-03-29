83050eed10570cd1928eaa34ab5d9b53acb98ff6
Curtis McEnroe <programble@gmail.com>
2016-03-29 17:12:14 -0400

    Change view to index_totals

--- views/index.sql
+++ views/index.sql
@@ -1,9 +1,12 @@
-CREATE OR REPLACE VIEW pgdv.index_total_size AS
-  SELECT pg_size_pretty(sum(pg_class.relpages::bigint * 8192)) AS size
+CREATE OR REPLACE VIEW pgdv.index_totals AS
+  SELECT
+    count(*) AS count,
+    sum(pg_class.relpages) AS pages,
+    pg_size_pretty(sum(pg_class.relpages::bigint * 8192)) AS size
   FROM pg_class
   LEFT JOIN pg_namespace ON (pg_namespace.oid = pg_class.relnamespace)
   WHERE pg_class.relkind = 'i'
     AND pg_namespace.nspname NOT IN ('pg_catalog', 'information_schema')
     AND pg_namespace.nspname !~ '^pg_toast';
 
-COMMENT ON VIEW pgdv.index_total_size IS 'Shows the total size of all indexes.';
+COMMENT ON VIEW pgdv.index_totals IS 'Shows the total number and size of all indexes.';
