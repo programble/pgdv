0673e6a169462d97e7f2f7127737b43956f9e902
Curtis McEnroe <programble@gmail.com>
2016-03-30 13:52:17 -0400

    Add view index_usage

--- views/index.sql
+++ views/index.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE VIEW pgdv.index_totals AS
     AND pg_namespace.nspname NOT IN ('pg_catalog', 'information_schema')
     AND pg_namespace.nspname !~ '^pg_toast';
 
-COMMENT ON VIEW pgdv.index_totals IS 'Shows the total number and size of all indexes.';
+COMMENT ON VIEW pgdv.index_totals IS 'total number and size of all indexes';
 
 CREATE OR REPLACE VIEW pgdv.index_size AS
   SELECT
@@ -23,4 +23,19 @@ CREATE OR REPLACE VIEW pgdv.index_size AS
     AND pg_namespace.nspname !~ '^pg_toast'
   ORDER BY pg_class.relpages DESC;
 
-COMMENT ON VIEW pgdv.index_size IS 'Shows the size of each index.';
+COMMENT ON VIEW pgdv.index_size IS 'size of each index';
+
+CREATE OR REPLACE VIEW pgdv.index_usage AS
+  SELECT
+    relname AS table,
+    n_live_tup AS rows,
+    seq_scan AS seq_scans,
+    idx_scan AS index_scans,
+    CASE idx_scan
+      WHEN 0 THEN NULL
+      ELSE (idx_scan::float / (seq_scan + idx_scan) * 100)::numeric(5, 2)
+    END AS percent_index_scan
+  FROM pg_stat_user_tables
+  ORDER BY rows DESC;
+
+COMMENT ON VIEW pgdv.index_usage IS 'index vs. seq scan usage for each table';
