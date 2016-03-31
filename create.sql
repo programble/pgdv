336df4d4e7ccad9f576f5ca9ba0f24de215875ab
Curtis McEnroe <programble@gmail.com>
2016-03-31 15:14:21 -0400

    Sort \ir commands in create.sql

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS pgdv;
-COMMENT ON SCHEMA pgdv IS 'PostgreSQL diagnostic views.';
+COMMENT ON SCHEMA pgdv IS 'PostgreSQL diagnostic views';
 
 \ir views/index.sql
-\ir views/table.sql
-\ir views/query.sql
 \ir views/mandelbrot.sql
+\ir views/query.sql
+\ir views/table.sql
