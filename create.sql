112680f6784a229980776ae5d7e76e6f376b7972
Curtis McEnroe <programble@gmail.com>
2016-03-31 16:00:53 -0400

    Add view views

--- create.sql
+++ create.sql
@@ -5,3 +5,14 @@ COMMENT ON SCHEMA pgdv IS 'PostgreSQL diagnostic views';
 \ir views/mandelbrot.sql
 \ir views/query.sql
 \ir views/table.sql
+
+CREATE OR REPLACE VIEW pgdv.views AS
+  SELECT
+    pg_class.relname AS view,
+    obj_description(pg_class.oid, 'pg_class') AS description
+  FROM pg_class, pg_namespace
+  WHERE pg_namespace.oid = pg_class.relnamespace
+    AND pg_namespace.nspname = 'pgdv'
+  ORDER BY view;
+
+COMMENT ON VIEW pgdv.views IS 'view of views';
